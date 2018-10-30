b68d9eeca674c995ca9f9c268e025339199a555a
Rails Udemy Course <railsudemycourse@gmail.com>
2018-10-30 14:21:56 +0000

    Lookup Stock Details

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,12 @@
 class Stock < ActiveRecord::Base
+
+  def self.new_from_lookup(ticker_symbol)
+    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+    #price = strip_commas(looked_up_stock.latest_price)
+    new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+  end
+  
+  #def self.strip_commas(number)
+    #number.gsub(".", "") #substitui o primeiro valor pelo segundo, se o valor dado for uma string
+  #end
 end
