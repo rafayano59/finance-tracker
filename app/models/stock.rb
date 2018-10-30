971c1666b0a79f0ec42061cbe55d389b5ab6c67f
Rails Udemy Course <railsudemycourse@gmail.com>
2018-10-30 14:52:02 +0000

    invalid search results page added

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,9 +1,13 @@
 class Stock < ActiveRecord::Base
 
   def self.new_from_lookup(ticker_symbol)
-    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-    #price = strip_commas(looked_up_stock.latest_price)
-    new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+    begin
+      looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+      #price = strip_commas(looked_up_stock.latest_price)
+      new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+    rescue Exception => e
+      return nil
+    end
   end
   
   #def self.strip_commas(number)
